08e160976079ef936dadde27655315e07bf46236
lusama02 <analu2007mel@gmail.com>
2026-01-30 14:34:56 -0300

    refactor(dtos): migra os dtos de 'Servidor' para 'Biblioteca'

--- MinhaCantina.Servidor/Controllers/ProdutoController.cs
+++ MinhaCantina.Servidor/Controllers/ProdutoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MinhaCantina.Biblioteca.DTOs;
 using MinhaCantina.Biblioteca.Modelos;
 using MinhaCantina.Servidor.Dados;
 using MySqlConnector;
@@ -167,17 +168,3 @@ public class ProdutoController(MinhaCantinaContexto cantinaContexto) : Controlle
 	}
 }
 
-public class ProdutoRegistroDto
-{
-	public string Nome { get; set; }
-	public decimal Preco { get; set; }
-	public string? Descricao { get; set; }
-	public int CategoriaId { get; set; }
-}
-public class ProdutoRespostaDto
-{
-	public int Id { get; set; }
-	public string Nome { get; set; }
-	public decimal Preco { get; set; }
-	public string CategoriaNome { get; set; }
-}
